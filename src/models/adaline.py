15ca566be447b665e4af05d6a625c69f5e26655a
guilhermedom <guilhermedfs@gmail.com>
2023-01-14 23:45:31 -0300

    style: rename function variables for readability

--- src/models/adaline.py
+++ src/models/adaline.py
@@ -15,7 +15,7 @@ class Adaline():
         self.weights = np.zeros(1 + X.shape[1])
         self.cost_list = []
 
-        for i in range(self.iterations):
+        for _ in range(self.iterations):
             predicted_value = self.linear_activation(X)
 
             errors = (y - predicted_value)
@@ -36,13 +36,12 @@ class Adaline():
 
 def create_input(n, noise):
     dataset_matrix = []
-    flat_dataset = []
     y_matrix = []
     dataset = []
 
     for _ in range(0, n):
         #a = np.full((5, 5), -1, dtype=int)
-        Y = [[1, -1, -1, -1, 1],
+        y = [[1, -1, -1, -1, 1],
              [-1, 1, -1, 1, -1],
              [-1, -1, 1, -1, -1],
              [-1, -1, 1, -1, -1],
@@ -52,16 +51,16 @@ def create_input(n, noise):
         for _ in range(0, noise):
             pos_x = np.random.randint(0, 5)
             pos_y = np.random.randint(0, 5)
-            Y[pos_x][pos_y] = -1
+            y[pos_x][pos_y] = -1
 
-        dataset_matrix.append(Y)
+        dataset_matrix.append(y)
         y_matrix.append(1)
 
-        flat_list = [item for sublist in Y for item in sublist]
+        flat_list = [item for sublist in y for item in sublist]
         flat_list.append(1)
         dataset.append(flat_list)
 
-        Y_inv = [[-1, -1, 1, -1, -1],
+        y_inv = [[-1, -1, 1, -1, -1],
                  [-1, -1, 1, -1, -1],
                  [-1, -1, 1, -1, -1],
                  [-1, 1, -1, 1, -1],
@@ -70,12 +69,12 @@ def create_input(n, noise):
         for _ in range(0, noise):
             pos_x = np.random.randint(0, 5)
             pos_y = np.random.randint(0, 5)
-            Y_inv[pos_x][pos_y] = -1
+            y_inv[pos_x][pos_y] = -1
 
-        dataset_matrix.append(Y)
+        dataset_matrix.append(y)
         y_matrix.append(-1)
 
-        flat_list = [item for sublist in Y_inv for item in sublist]
+        flat_list = [item for sublist in y_inv for item in sublist]
         flat_list.append(-1)
         dataset.append(flat_list)
 
@@ -89,26 +88,25 @@ def train_test_split(dataset, percentage):
     np.random.shuffle(dataset)
 
     index_train_x = math.floor(percentage * dataset.shape[0])
-    #index_train_y = dataset.shape[0] - math.floor(percentage * dataset.shape[0])
 
     train = dataset[:index_train_x, :]
-    train_y = train[:, -1]
-    train_x = train[:, :-1]
+    y_train = train[:, -1]
+    X_train = train[:, :-1]
 
     test = dataset[index_train_x:, :]
-    test_y = test[:, -1]
-    test_x = test[:, :-1]
+    y_test = test[:, -1]
+    X_test = test[:, :-1]
 
-    return (train_x, train_y, test_x, test_y)
+    return (X_train, y_train, X_test, y_test)
 
-def accuracy_score(pred_y, test_y):
+def accuracy_score(y_pred, y_test):
     acc_counter = 0
-    print(pred_y.shape[0])
+    print(y_pred.shape[0])
 
-    for i in range(0, pred_y.shape[0]):
-        if pred_y[i] == test_y[i]:
+    for i in range(0, y_pred.shape[0]):
+        if y_pred[i] == y_test[i]:
             acc_counter += 1
-    return 1/(pred_y.shape[0]) * acc_counter
+    return 1 / (y_pred.shape[0]) * acc_counter
 
 dataset = create_input(n = 50, noise = 5)
 
